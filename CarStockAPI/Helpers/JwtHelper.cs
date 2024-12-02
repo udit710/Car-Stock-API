8a1031325ec7478c7539f3a2239638c5557946b4
udit710 <s3933905@student.rmit.edu.au>
2024-12-03 00:53:20 +1100

    Jwt Helper Added

---
+++ CarStockAPI/Helpers/JwtHelper.cs
@@ -0,0 +1,48 @@
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+using System.Security.Cryptography;
+
+namespace CarStockAPI.Helpers
+{
+    public static class JwtHelper
+    {
+        public static string GenerateToken(int dealerId, string secretKey)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var claims = new List<Claim>
+            {
+                new Claim("dealerId", dealerId.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(keyBytes),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+
+        public static string GenerateSecureKey(int keySizeInBits = 256)
+        {
+            int keySizeInBytes = keySizeInBits / 8;
+            var keyBytes = new byte[keySizeInBytes];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(keyBytes);
+            }
+            return Convert.ToBase64String(keyBytes);
+        }
+    }
+
+}
