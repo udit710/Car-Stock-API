1aaec33913d8ef8ac3a092a408b4972e6c95b1fe
udit710 <s3933905@student.rmit.edu.au>
2024-12-02 14:02:37 +1100

    Added Data Models

---
+++ CarStockAPI/Models/Car.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CarStockAPI.Models{
+    public class Car{
+        public int CarId { get; set; }
+        public int DealerId { get; set; }
+
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int StockCount { get; set; }
+    }
+}
---
+++ CarStockAPI/Models/Dealer.cs
@@ -0,0 +1,8 @@
+namespace CarStockAPI.Models{
+    public class Dealer{
+        public int DealerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
