7f27f23438663a501eb651b7db4f07fa11a8ee28
udit710 <s3933905@student.rmit.edu.au>
2024-12-03 15:02:52 +1100

    Completed Authorization Using Jwt

--- CarStockAPI/Models/Requests/AuthRequest.cs
+++ CarStockAPI/Models/Requests/AuthRequest.cs
@@ -11,8 +11,8 @@ namespace CarStockAPI.Models.Requests
     {
         public AuthRequestValidator()
         {
-            RuleFor(x => x.Username).NotEmpty();
-            RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
+            RuleFor(x => x.Username).NotEmpty().MinimumLength(3).MaximumLength(20);
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(6).MaximumLength(20);
         }
     }
 }
